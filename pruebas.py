06a12ccaf88707d6af88f86f084ed9238e0d1edb
AguilarSantiago <62967452+AguilarSantiago@users.noreply.github.com>
2020-05-28 21:54:20 -0300

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,32 +1,4 @@
 <<<<<<< HEAD
-import collections
-
-#función que corrobora que el número ingresado este en el rango correcto
-def rango (num):
-    if num < 1 or num > 9999:
-        print("Debe ingresar un número entre 1 y 9999")
-        return False
-    else:
-        return True 
- 
-#función que cuenta las iteraciones 
-def kaprekar(num):        
-    numS = str(num)
-
-    # Agrega los 0 necesarios
-    numS = '0' * (4 - len(numS)) + numS    
-
-    # Tiene que haber 2 digitos diferentes por lo menos, sino arroja 8
-    if len(collections.Counter(numS)) == 1:
-        print("8 iteraciones.")
-        return 8
-
-    n = 0
-
-=======
-
-# -*- coding: utf-8 -*-
-import collections
 
 # función que corrobora que el número ingresado este en el rango correcto
 def rango (num):
@@ -79,4 +51,4 @@ def kaprekar(num):
 
         print(n," iteraciones.")
         return n
->>>>>>> Boschi
+
