f591d534552edf2c8b281b0ceafa0241ff21595e
spalenzam <53008316+spalenzam@users.noreply.github.com>
2020-05-28 22:05:20 -0300

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,34 +1,19 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-# función que corrobora que el número ingresado este en el rango correcto
-=======
 import collections
 
 #función que corrobora que el número ingresado este en el rango correcto
->>>>>>> Spalenza-
 def rango (num):
     if num < 1 or num > 9999:
         print("Debe ingresar un número entre 1 y 9999")
         return False
     else:
         return True 
- 
-<<<<<<< HEAD
+    
 # función que cuenta las iteraciones 
 def kaprekar(num):
     numS = str(num)
 
     # Agrega los 0 necesarios
     numS = '0' * (4 - len(numS)) + numS
-=======
-#función que cuenta las iteraciones 
-def kaprekar(num):        
-    numS = str(num)
-
-    # Agrega los 0 necesarios
-    numS = '0' * (4 - len(numS)) + numS    
->>>>>>> Spalenza-
 
     # Tiene que haber 2 digitos diferentes por lo menos, sino arroja 8
     if len(collections.Counter(numS)) == 1:
@@ -36,17 +21,12 @@ def kaprekar(num):
         return 8
 
     n = 0
-<<<<<<< HEAD
-
-    # si el número ingresado es 6174 arroja el número 0
-=======
     
     #si el número ingresado es 6174 arroja el número 0
->>>>>>> Spalenza-
     if num ==6174:
         print("0 iteración." )
         return 0
-
+    
     else:
         while (num != 6174):
             n += 1
@@ -61,17 +41,10 @@ def kaprekar(num):
 
             # ordena los número de manera ascendente
             menor = int(''.join(sorted(numS)))
-<<<<<<< HEAD
 
             # resta el mayor menos el menor
             num = mayor - menor
-
-=======
            
-            # resta el mayor menos el menor        
-            num = mayor - menor
-           
->>>>>>> Spalenza-
             if n == 8:
                 print("Error")
                 return -1
@@ -79,8 +52,6 @@ def kaprekar(num):
         print(n," iteraciones.")
         return n
 
-<<<<<<< HEAD
-=======
 #MAIN
 while True:
     numero = int(input("Ingresa el numero de casos de prueba: "))
@@ -89,4 +60,4 @@ while True:
 
 kaprekar (numero)
 
->>>>>>> Spalenza-
+
